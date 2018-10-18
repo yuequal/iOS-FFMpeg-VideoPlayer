f466ef048d5235c738a824dcba08f26ddaf89d26
yuxueqing <your@email>
2018-10-18 18:35:51 +0800

    iOS-VideoPlayer

--- ffmpeg_demo/Controller/AVAudioRender.h
+++ ffmpeg_demo/Controller/AVAudioRender.h
@@ -7,12 +7,85 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+#import "AVModeFrame.hpp"
+
+@class AVAudioNotificationObserver;
+@protocol AVAudioDataProtocol;
 
 namespace AVVideoPlayer {
     
+    class AVAudioRenderImpl
+    {
+    public:
+        static std::unique_ptr<AVAudioRenderImpl> Create(id<AVAudioDataProtocol> protocol,float sampleRates);
+        
+        virtual ~AVAudioRenderImpl() = default;
+        
+        virtual void Start() = 0;
+        virtual void Stop() = 0;
+        virtual bool IsPlaying() = 0;
+        
+        virtual float SampleRates() const = 0;
+    public:
+        
+        static float HWSamplerates();
+        
+        static unsigned long MaxChannels();
+    };
     
+    class AVAudioRender  : public AVAudioRenderImpl , public std::enable_shared_from_this<AVAudioRender>
+    {
+    public:
+        AVAudioRender(id<AVAudioDataProtocol> protocol, float sampleRates);
+        virtual ~AVAudioRender();
+        
+    public:
+        
+        virtual void Start();
+        virtual void Stop();
+        virtual bool IsPlaying();
+        virtual float SampleRates() const;
+        
+        virtual void SetIsPlaying(bool);
+        
+        friend float AVAudioRenderImpl::HWSamplerates();
+        
+    public:
+        void HandleNotification(NSNotification *notification);
+        
+    public:
+        static OSStatus RenderData(void *userData, AudioBufferList *dataBuffer,AudioUnitRenderActionFlags *flags,const AudioTimeStamp *timeStamp ,UInt32 element, UInt32 frames);
+        
+    private:
+        
+        float m_sampleRate { 0 };
+        bool m_isPlaying { false };
+        SInt16 *m_outData;
+        
+        AUGraph *m_audioGraph ;
+        AUNode *m_audioNode;
+        AudioUnit *m_audioUint;
+        AUNode *m_convertNode;
+        AudioUnit *m_convertUint;
+        
+        id<AVAudioDataProtocol> m_dataProtocol;
+        AVAudioNotificationObserver *m_observer;
+    };
 }
 
-@interface AVAudioRender : NSObject
+@protocol AVAudioDataProtocol <NSObject>
+
+- (NSInteger)audioData:(SInt16*)sampleBuffer frames:(NSInteger)frames channels:(NSInteger)channels;
 
 @end
+
+@interface AVAudioNotificationObserver : NSObject
+{
+    std::weak_ptr<AVVideoPlayer::AVAudioRender> m_audioRender;
+}
+
+- (instancetype)initWithAudioRender:(const std::shared_ptr<AVVideoPlayer::AVAudioRender>&)render;
+
+@end
+
--- ffmpeg_demo/Controller/AVAudioRender.mm
+++ ffmpeg_demo/Controller/AVAudioRender.mm
@@ -8,6 +8,84 @@
 
 #import "AVAudioRender.h"
 
-@implementation AVAudioRender
+namespace AVVideoPlayer {
+    
+AVAudioRender::AVAudioRender(id<AVAudioDataProtocol> protocol ,float sampleRates)
+: m_dataProtocol(protocol)
+, m_sampleRate(sampleRates)
+{
+    std::shared_ptr<AVAudioRender> render = shared_from_this();
+    AVAudioNotificationObserver *observer = [[AVAudioNotificationObserver alloc] initWithAudioRender:render];
+    m_observer = observer;
+}
+
+AVAudioRender::~AVAudioRender()
+{
+    
+}
+
+void AVAudioRender::Start()
+{
+    
+}
+
+void AVAudioRender::Stop()
+{
+    
+}
+
+bool AVAudioRender::IsPlaying()
+{
+    return m_isPlaying;
+}
+
+float AVAudioRender::SampleRates() const
+{
+    return m_sampleRate;
+}
+
+void AVAudioRender::SetIsPlaying(bool isPlaying)
+{
+    m_isPlaying = isPlaying;
+}
+
+float AVAudioRenderImpl::HWSamplerates()
+{
+    return 0;
+}
+
+void AVAudioRender::HandleNotification(NSNotification *notification)
+{
+    
+}
+    
+}
+
+@implementation AVAudioNotificationObserver
+
+- (instancetype)initWithAudioRender:(const std::shared_ptr<AVVideoPlayer::AVAudioRender> &)render
+{
+    self = [super init];
+    if (self) {
+        m_audioRender = render;
+        [self _registerInteruptNotification];
+    }
+    return self;
+}
+
+- (void)_registerInteruptNotification
+{
+    [[NSNotificationCenter defaultCenter] addObserverForName:@"AVAudioSessionInterruptionNotification" object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+        auto audioRender = self->m_audioRender.lock();
+        if (audioRender)
+            audioRender->HandleNotification(note);
+    }];
+}
+
+- (void)_removeInteruptNotification
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"AVAudioSessionInterruptionNotification" object:nil];
+}
 
 @end
+
